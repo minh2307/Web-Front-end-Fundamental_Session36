4b09490aa65bff25fadf410e34c05ed1f06283b7
minh2307 <minh23923@gmail.com>
2025-03-28 08:50:09 +0700

    done

---
+++ ss36-bt7.js
@@ -0,0 +1,34 @@
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+let userName = document.getElementById("userName");
+let userPassWord = document.getElementById("userPassWord");
+
+function logIn() {
+  let user = users.find(
+    (el) => el.user === userName.value && el.passWord === userPassWord.value
+  );
+
+  if (user) {
+    alert("Đăng nhập thành công!");
+    location.href = "ss36-bt7-home.html";
+  } else {
+    alert("Tên đăng nhập hoặc mật khẩu không đúng!");
+  }
+}
+
+function register() {
+  let userExist = users.some((el) => el.user === userName.value);
+
+  if (userExist) {
+    alert("Tên đăng nhập đã tồn tại!");
+  } else {
+    users.push({
+      user: userName.value,
+      passWord: userPassWord.value,
+    });
+
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Đăng ký thành công! Mời bạn đăng nhập.");
+    location.href = "ss36-bt7-logIn.html";
+  }
+}
